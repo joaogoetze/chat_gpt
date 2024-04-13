0fdd58ce1e6b8fefeebeda193667b483c1c39bd8
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-04-13 18:55:24 -0300

    Creating chat screen and set screen configuration

--- app/src/main/java/com/example/chat_gpt/MainActivity.kt
+++ app/src/main/java/com/example/chat_gpt/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.chat_gpt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chat_gpt.databinding.ActivityMainBinding
+import view.ChatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNewChat.setOnClickListener{
+            val goOnChatIntent = Intent(this, ChatActivity::class.java)
+            startActivity(goOnChatIntent)
+        }
     }
 }
